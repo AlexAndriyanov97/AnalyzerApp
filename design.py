1c614cc97f37a4151345db6ae1cb8fef0df67bf3
AlexAndriyanov97 <alexandriyanov97@gmail.com>
2018-09-27 06:43:02 +0700

    add fullfuncional

--- design.py
+++ design.py
@@ -1,12 +1,20 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QTableWidgetItem
+
+import table
 
 
 class Ui_MainWindow(object):
+    def openWindows(self, dataframe):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = table.Ui_OtherWindow(self.window, dataframe)
+        self.window.setWindowTitle("Подробно")
+        self.window.show()
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(520, 494)
+        MainWindow.resize(700, 600)
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralWidget)
@@ -39,11 +47,10 @@ class Ui_MainWindow(object):
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
         header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
 
-
         self.progressBar = QtWidgets.QProgressBar(self.splitter)
-        self.progressBar.setProperty("value", 0)
+        self.progressBar.setValue(0)
         self.progressBar.setObjectName("progressBar")
-        self.progressBar.setVisible(False)
+        self.progressBar.setVisible(True)
 
         self.pushButton = QtWidgets.QPushButton(self.splitter)
         self.pushButton.setObjectName("pushButton")
@@ -53,6 +60,26 @@ class Ui_MainWindow(object):
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
+
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.tab_2)
+        self.horizontalLayout.setContentsMargins(11, 11, 11, 11)
+        self.horizontalLayout.setSpacing(6)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_3.setSpacing(6)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+
+        self.tableWidget_2 = QtWidgets.QTableWidget(self.tab_2)
+        self.tableWidget_2.setObjectName("tableWidget_2")
+        self.tableWidget_2.setColumnCount(0)
+        self.tableWidget_2.setRowCount(0)
+        self.verticalLayout_3.addWidget(self.tableWidget_2)
+
+        self.pushButton_3 = QtWidgets.QPushButton(self.tab_2)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.verticalLayout_3.addWidget(self.pushButton_3)
+        self.horizontalLayout.addLayout(self.verticalLayout_3)
+
         self.tabWidget.addTab(self.tab_2, "")
         self.verticalLayout.addWidget(self.tabWidget)
         MainWindow.setCentralWidget(self.centralWidget)
@@ -61,11 +88,24 @@ class Ui_MainWindow(object):
         self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+    def drawTable(self, df):
+        self.tableWidget_2.setColumnCount(len(df.columns))
+        self.tableWidget_2.setRowCount(len(df.index))
+
+        for i in range(len(df.index)):
+            for j in range(len(df.columns)):
+                self.tableWidget_2.setItem(i, j, QTableWidgetItem(str(df.iat[i, j])))
+        self.tableWidget_2.setHorizontalHeaderLabels(
+            ['Имя столбца', 'Частота встречаемости (%)', 'Средний процент плохих данных'])
+        self.tableWidget_2.resizeColumnsToContents()
+        self.tableWidget_2.resizeRowsToContents()
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Анализатор"))
         self.checkBox.setText(_translate("MainWindow", "Выделить всё"))
         self.pushButton.setText(_translate("MainWindow", "Выберите папку"))
         self.pushButton_2.setText(_translate("MainWindow", "Анализировать"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Tab 1"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Tab 2"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Главная страница"))
+        self.pushButton_3.setText(_translate("MainWindow", "Обновить"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Общая статистика"))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 import lasio
+import pandas as pd
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QTableWidgetItem
 
@@ -13,6 +14,9 @@ class AnalyzerApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         # Это здесь нужно для доступа к переменным, методам
         # и т.д. в файле design.py
         super().__init__()
+        self._final_stat = {}
+        self._detailed_dictionary = {}
+        self._percent_bad_data = {}
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         self._directory = None
         self.pushButton.clicked.connect(self.browse_folder)  # Выполнить функцию browse_folder
@@ -20,6 +24,22 @@ class AnalyzerApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.pushButton_2.clicked.connect(self.analyzer)
         self.checkBox.stateChanged.connect(self.clickBox)
         self.count_files = 0
+        self.tableWidget.clicked.connect(self.on_click)
+
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            if currentQTableWidgetItem.column() == 0:
+                if self._detailed_dictionary.get(currentQTableWidgetItem.text()) is not None:
+                    self.openWindows(dataframe=self._detailed_dictionary[currentQTableWidgetItem.text()])
+            if currentQTableWidgetItem.column() == 1:
+                if self._percent_bad_data.get(
+                        (self.tableWidget.item(currentQTableWidgetItem.row(), 0)).text()) is not None:
+                    self.openWindows(dataframe=pd.DataFrame.from_dict(
+                        self._percent_bad_data[(self.tableWidget.item(currentQTableWidgetItem.row(), 0)).text()],
+                        orient='index', columns=['% плохих данных']
+                    ))
+        self.tableWidget.clearSelection()
 
     def clickBox(self, state):
         if state == QtCore.Qt.Checked:
@@ -30,15 +50,23 @@ class AnalyzerApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 self.tableWidget.item(iter, 2).setCheckState(QtCore.Qt.Unchecked)
 
     def browse_folder(self):
-        self.tableWidget.clear()  # На случай, если в списке уже есть элементы
+        tmp = self._directory
         self._directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
         # открыть диалог выбора директории и установить значение переменной
         # равной пути к выбранной директории
+        self.tableWidget.setHorizontalHeaderLabels(["Имя", "Процент плохих данных", "Используемые файлы"])
+        if self._directory == '':
+            self._directory = tmp
+            return
+        self.tableWidget.clear()
         iterration = 0
         self.count_files = len(
             [name for name in os.listdir(self._directory) if os.path.isfile(os.path.join(self._directory, name))])
         self.tableWidget.setRowCount(self.count_files)
         if self._directory:  # не продолжать выполнение, если пользователь не выбрал директорию
+            self._detailed_dictionary = {}
+            self._percent_bad_data = {}
+            self._final_stat = {}
             for file_name in os.listdir(self._directory):  # для каждого файла в директории
                 self.tableWidget.setItem(iterration, 0, QTableWidgetItem(file_name))  # добавить файл в listWidget
                 self.tableWidget.setItem(iterration, 1, QTableWidgetItem(""))
@@ -48,23 +76,28 @@ class AnalyzerApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                 self.tableWidget.setItem(iterration, 2, chkBoxItem)
                 iterration += 1
         self.tableWidget.setHorizontalHeaderLabels(["Имя", "Процент плохих данных", "Используемые файлы"])
+        self.checkBox.setChecked(False)
         self.checkBox.setVisible(True)
+        self.progressBar.setValue(0)
 
     def analyzer(self):
         first_file = True
         my_dict = {}
+        average_percent_bad_data = {}
         bad_files = []
-        i = 0
+        i = -1
+        self.progressBar.setVisible(True)
         if self._directory:
             for name in os.listdir(self._directory):
+                i += 1
+                self.progressBar.setValue(round(i / self.count_files * 100))
                 if self.tableWidget.item(i, 2).checkState():
-                    print(name)
                     if os.path.isfile(os.path.join(self._directory, name)):
                         try:
                             las = lasio.read(self._directory + "/" + name)
                         except ValueError:
                             bad_files.append(name)
-                            i += 1
+                            self.tableWidget.setItem(i, 1, QTableWidgetItem('невозможно прочитать файл'))
                             continue
                         if first_file:
                             my_dict = dict.fromkeys(las.keys(), 0)
@@ -77,16 +110,29 @@ class AnalyzerApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
                                 my_dict[item] += 1
                             else:
                                 my_dict[item] = 1
-                            percent_bad_value[item] = round(count_NaN[item] / las[item].size, 2)
+                            percent_bad_value[item] = round(count_NaN[item] / las[item].size * 100)
+                            if item in average_percent_bad_data.keys():
+                                average_percent_bad_data[item] += percent_bad_value[item]
+                            else:
+                                average_percent_bad_data[item] = 0
                             tmp += percent_bad_value[item]
-                        self.tableWidget.setItem(i, 1, QTableWidgetItem(str(round(tmp / len(percent_bad_value), 2))))
-                        print(i)
-                    i += 1
+                        percent_bad_value = dict(sorted(percent_bad_value.items(), key=lambda x: x[1], reverse=True))
+                        self._percent_bad_data[name] = percent_bad_value
+                        self.tableWidget.setItem(i, 1, QTableWidgetItem(str(round(tmp / len(percent_bad_value)))))
+                        self._detailed_dictionary[name] = las.df().describe()
+                        my_dict['DEPT'] += 1
                 else:
-                    i += 1
                     continue
-                my_dict['DEPT'] = self.count_files
-                print(sorted(my_dict.items(), key=lambda x: x[1], reverse=True))
+            i += 1
+            for item in average_percent_bad_data.keys():
+                average_percent_bad_data[item] = round(average_percent_bad_data[item] / my_dict[item])
+                my_dict[item] = round(my_dict[item] / my_dict['DEPT'] * 100)
+                self._final_stat[item] = [item, my_dict[item], average_percent_bad_data[item]]
+            self._final_stat['DEPT'] = ['DEPT', 100, 0]
+
+            self._final_stat = dict(sorted(self._final_stat.items(), key=lambda x: (x[1])[1], reverse=True))
+            self.drawTable(pd.DataFrame.from_dict(self._final_stat, orient='index'))
+            self.progressBar.setValue(round(i / self.count_files * 100))
 
 
 def main():
